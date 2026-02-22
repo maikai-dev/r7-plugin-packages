5987a91f25b83117c10141928467486e0e33a45f
your_username <tanialena02@gmail.com>
2026-02-23 00:07:53 +0300

    fix: use GitHub Pages URL for all marketplace URLs, remove local fallbacks

--- store/plugin/scripts/code.js
+++ store/plugin/scripts/code.js
@@ -15,8 +15,8 @@
  * limitations under the License.
  *
  */
-(function(window, undefined) {
-	const isLocal = ( (window.AscDesktopEditor !== undefined) && (window.location.protocol.indexOf('file') !== -1) );
+(function (window, undefined) {
+	const isLocal = ((window.AscDesktopEditor !== undefined) && (window.location.protocol.indexOf('file') !== -1));
 	let interval = null;
 	let errTimeout = null;
 	let loader = null;
@@ -32,16 +32,16 @@
 	let BPluginReady = false;
 	let editorVersion = null;
 	let marketplaceURl = null;
-	const OOMarketplaceUrl = isLocal ? './store/index.html' : 'https://onlyoffice.github.io/store/index.html';
+	const OOMarketplaceUrl = 'https://maikai-dev.github.io/r7-plugin-packages/store/index.html';
 	try {
 		// for incognito mode
 		marketplaceURl = localStorage.getItem('DeveloperMarketplaceUrl') || OOMarketplaceUrl;
 	} catch (err) {
 		marketplaceURl = OOMarketplaceUrl;
 	}
-	
-	window.Asc.plugin.init = function() {
-		window.Asc.plugin.executeMethod('ShowButton',['developer', true, 'right']);
+
+	window.Asc.plugin.init = function () {
+		window.Asc.plugin.executeMethod('ShowButton', ['developer', true, 'right']);
 		// resize window
 		window.Asc.plugin.resizeWindow(608, 600, 608, 600, 0, 0);
 		if (!isLocal) {
@@ -62,18 +62,18 @@
 			document.getElementById('notification').classList.remove('hidden');
 
 		// send message that plugin is ready
-		window.Asc.plugin.executeMethod('GetVersion', null, function(version) {
+		window.Asc.plugin.executeMethod('GetVersion', null, function (version) {
 			editorVersion = version;
 			BPluginReady = true;
 			if (BFrameReady)
-				postMessage( { type: 'PluginReady', version: editorVersion } );
+				postMessage({ type: 'PluginReady', version: editorVersion });
 		});
 
 		let divNoInt = document.getElementById('div_noIternet');
 		let style = document.getElementsByTagName('head')[0].lastChild;
 		let pageUrl = marketplaceURl;
 		iframe.src = pageUrl + window.location.search;
-		iframe.onload = function() {
+		iframe.onload = function () {
 			BFrameReady = true;
 			if (BPluginReady) {
 				if (!divNoInt.classList.contains('hidden')) {
@@ -81,13 +81,13 @@
 					clearInterval(interval);
 					interval = null;
 				}
-				postMessage( { type: 'Theme', theme: window.Asc.plugin.theme, style : style.innerHTML } );
-				postMessage( { type: 'PluginReady', version: editorVersion } );
+				postMessage({ type: 'Theme', theme: window.Asc.plugin.theme, style: style.innerHTML });
+				postMessage({ type: 'PluginReady', version: editorVersion });
 			}
 		};
 	};
 
-	window.Asc.plugin.button = function(id, windowID) {
+	window.Asc.plugin.button = function (id, windowID) {
 		if (warningWindow && warningWindow.id == windowID) {
 			switch (id) {
 				case 0:
@@ -97,7 +97,7 @@
 					removePlugin(true);
 					break;
 				default:
-					postMessage( {type: 'Removed', guid: ''} );
+					postMessage({ type: 'Removed', guid: '' });
 					break;
 			}
 			window.Asc.plugin.executeMethod('CloseWindow', [windowID]);
@@ -107,9 +107,9 @@
 			else
 				window.Asc.plugin.executeMethod('CloseWindow', [windowID]);
 		} else if (id == 'back') {
-			window.Asc.plugin.executeMethod('ShowButton',['back', false]);
+			window.Asc.plugin.executeMethod('ShowButton', ['back', false]);
 			if (iframe && iframe.contentWindow)
-				postMessage( { type: 'onClickBack' } );
+				postMessage({ type: 'onClickBack' });
 		} else if (id == 'developer') {
 			createWindow('developer');
 		} else {
@@ -117,68 +117,68 @@
 		}
 	};
 
-	window.addEventListener('message', function(message) {
+	window.addEventListener('message', function (message) {
 		// getting messages from marketplace
 		let data = JSON.parse(message.data);
-			
+
 		switch (data.type) {
 			case 'getInstalled':
 				// данное сообщение используется только при инициализации плагина и по умолчанию идёт парсинг и отрисовка плагинов из стора
 				// добавлен флаг updateInstalled - в этом случае не загружаем плагины из стора повторно, работаем только с установленными
-				
-				window.Asc.plugin.executeMethod('GetInstalledPlugins', null, function(result) {
-					postMessage({ type: 'InstalledPlugins', data: result, updateInstalled: data.updateInstalled } );
+
+				window.Asc.plugin.executeMethod('GetInstalledPlugins', null, function (result) {
+					postMessage({ type: 'InstalledPlugins', data: result, updateInstalled: data.updateInstalled });
 				});
 				break;
 			case 'install':
-				window.Asc.plugin.executeMethod('InstallPlugin', [data.config, data.guid], function(result) {
+				window.Asc.plugin.executeMethod('InstallPlugin', [data.config, data.guid], function (result) {
 					postMessage(result);
 				});
 				break;
 			case 'remove':
 				removeGuid = data.guid;
-				if ( Number( editorVersion.split('.').join('') < 740) )
+				if (Number(editorVersion.split('.').join('') < 740))
 					removePlugin(true);
-				else if ( !data.backup )
+				else if (!data.backup)
 					removePlugin(data.backup);
 				else
 					createWindow('warning');
 				break;
 			case 'update':
-				window.Asc.plugin.executeMethod('UpdatePlugin', [data.config, data.guid], function(result) {
+				window.Asc.plugin.executeMethod('UpdatePlugin', [data.config, data.guid], function (result) {
 					postMessage(result);
 				});
 				break;
-			case 'showButton' :
-				window.Asc.plugin.executeMethod('ShowButton',['back', true]);
+			case 'showButton':
+				window.Asc.plugin.executeMethod('ShowButton', ['back', true]);
 				break;
 		}
-		
+
 	}, false);
 
-	window.Asc.plugin.onExternalMouseUp = function() {
+	window.Asc.plugin.onExternalMouseUp = function () {
 		// mouse up event outside the plugin window
 		if (iframe && iframe.contentWindow) {
-			postMessage( { type: 'onExternalMouseUp' } );
+			postMessage({ type: 'onExternalMouseUp' });
 		}
 	};
 
-	window.Asc.plugin.onThemeChanged = function(theme) {
+	window.Asc.plugin.onThemeChanged = function (theme) {
 		// theme changed event
-		if ( theme.type.indexOf('light') !== -1 ) {
+		if (theme.type.indexOf('light') !== -1) {
 			theme['background-toolbar'] = '#fff';
 		}
 		window.Asc.plugin.onThemeChangedBase(theme);
 		let style = document.getElementsByTagName('head')[0].lastChild;
 		if (iframe && iframe.contentWindow)
-			postMessage( { type: 'Theme', theme: theme, style : style.innerHTML } );
+			postMessage({ type: 'Theme', theme: theme, style: style.innerHTML });
 	};
 
-	window.Asc.plugin.onTranslate = function() {
+	window.Asc.plugin.onTranslate = function () {
 		let label = document.getElementById('lb_notification');
 		if (label)
 			label.innerHTML = window.Asc.plugin.tr(label.innerHTML);
-		
+
 		label = document.getElementById('lb_noInternet');
 		if (label)
 			label.innerHTML = window.Asc.plugin.tr(label.innerHTML);
@@ -187,9 +187,9 @@
 	function checkInternet(bSetTimeout) {
 		try {
 			let xhr = new XMLHttpRequest();
-			let url = 'https://onlyoffice.github.io/store/translations/langs.json';
+			let url = 'https://maikai-dev.github.io/r7-plugin-packages/store/translations/langs.json';
 			xhr.open('GET', url, true);
-			
+
 			xhr.onload = function () {
 				if (this.readyState == 4) {
 					if (this.status >= 200 && this.status < 300) {
@@ -207,7 +207,7 @@
 			endInternetChecking(false);
 		}
 		if (bSetTimeout) {
-			errTimeout = setTimeout(function() {
+			errTimeout = setTimeout(function () {
 				// if loading is too long show the error (because sometimes requests can not send error)
 				endInternetChecking(false);
 			}, 15000);
@@ -223,7 +223,7 @@
 		} else {
 			document.getElementById('div_noIternet').classList.remove('hidden');
 			if (!interval) {
-				interval = setInterval(function() {
+				interval = setInterval(function () {
 					checkInternet(false);
 				}, 5000);
 			}
@@ -262,18 +262,18 @@
 		let location = window.location;
 		let start = location.pathname.lastIndexOf('/') + 1;
 		let file = location.pathname.substring(start);
-		
+
 		let variation = {
-			url : location.href.replace(file, fileName),
-			description : description,
-			isVisual : true,
-			isModal : true,
+			url: location.href.replace(file, fileName),
+			description: description,
+			isVisual: true,
+			isModal: true,
 			isViewer: true,
-			EditorsSupport : ['word', 'cell', 'slide', 'pdf'],
-			size : size,
-			buttons : buttons
+			EditorsSupport: ['word', 'cell', 'slide', 'pdf'],
+			size: size,
+			buttons: buttons
 		};
-		
+
 		if (type == 'warning') {
 			if (!warningWindow) {
 				warningWindow = new window.Asc.PluginWindow();
@@ -282,7 +282,7 @@
 		} else {
 			if (!developerWindow) {
 				developerWindow = new window.Asc.PluginWindow();
-				developerWindow.attachEvent('onWindowMessage', function(message) {
+				developerWindow.attachEvent('onWindowMessage', function (message) {
 					if (message.type == 'SetURL') {
 						if (message.url.length) {
 							marketplaceURl = message.url;
@@ -300,29 +300,29 @@
 			}
 			developerWindow.show(variation);
 		}
-		
+
 	};
 
 	function removePlugin(backup) {
 		if (removeGuid)
-			window.Asc.plugin.executeMethod('RemovePlugin', [removeGuid, backup], function(result) {
+			window.Asc.plugin.executeMethod('RemovePlugin', [removeGuid, backup], function (result) {
 				postMessage(result);
 			});
-		
+
 		removeGuid = null;
 	};
 
-	window.onresize = function() {
+	window.onresize = function () {
 		// zoom for all elements in plugin window
 		let zoom = 1;
 		if (window.devicePixelRatio < 1)
 			zoom = 1 / window.devicePixelRatio;
-		
+
 		document.getElementsByTagName('html')[0].style.zoom = zoom;
 	};
 
 	function createLoader() {
-		$('#loader-container').removeClass( 'hidden' );
+		$('#loader-container').removeClass('hidden');
 		loader && (loader.remove ? loader.remove() : $('#loader-container')[0].removeChild(loader));
 		loader = showLoader($('#loader-container')[0], window.Asc.plugin.tr('Checking internet connection...'));
 	};
@@ -330,7 +330,7 @@
 	function destroyLoader() {
 		clearTimeout(loaderTimeout);
 		loaderTimeout = null;
-		$('#loader-container').addClass( 'hidden' )
+		$('#loader-container').addClass('hidden')
 		loader && (loader.remove ? loader.remove() : $('#loader-container')[0].removeChild(loader));
 		loader = undefined;
 	};
